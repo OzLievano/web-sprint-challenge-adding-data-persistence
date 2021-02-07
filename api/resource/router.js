c7aa49bd529e3b3bf7ad3ba7fcc3897109d8f40e
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-07 17:10:28 -0500

    resource router created

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,2 +1,34 @@
 // build your `/api/resources` router here
 const Resources = require('./model')
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',function getAllResources(req,res){
+    Resources.getResources()
+        .then((resources)=>{
+            res.status(200).json(resources)
+        })
+        .catch((err)=>{
+            res.status(500).json({error:err.message})
+        })
+})
+
+router.post('/',function createResources(req,res){
+    const newResource = req.body;
+
+    if(!newResource.resource_name){
+        res.status(400).json({error:"please include a resource name"})
+    }else{
+        Resources.createResource(newResource)
+        .then((resources)=>{
+            res.status(201).json(newResource)
+        })
+        .catch((err)=>{
+            res.status(500).json({error:err.message})
+        })
+    }
+})
+
+module.exports = router;
