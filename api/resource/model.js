471c8c6cc073c39e9230bc68870e85f9c05efb08
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-07 15:51:46 -0500

    created createResource function for resource endpoint

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,11 @@
 // build your `Resource` model here
+const db = require('../../data/db-config');
+
+
+module.exports = {
+    createResource
+}
+
+async function createResource(data){
+    return await db('resources').insert(data);
+}
