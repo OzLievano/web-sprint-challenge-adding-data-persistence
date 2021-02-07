fed006f07c63c371b535ff8a99a96319d853f9a4
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-07 15:12:13 -0500

    created server and created server script on package.json

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // start your server here
-const myName = "ozzy";
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, function checkIfPortIsListening(){
+    console.log(`Listening on port ${PORT}...`)
+})
